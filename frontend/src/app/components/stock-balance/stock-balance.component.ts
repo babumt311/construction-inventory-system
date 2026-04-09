33b2fb40bee5c378c8468cd4a1b4eff8fbdff706
babumt311 <146863988+babumt311@users.noreply.github.com>
2026-04-10 01:57:12 +0530

    Update stock-balance.component.ts

--- frontend/src/app/components/stock-balance/stock-balance.component.ts
+++ frontend/src/app/components/stock-balance/stock-balance.component.ts
@@ -194,8 +194,7 @@ export class StockBalanceComponent implements OnInit, OnDestroy {
     let loaded = 0;
     const filters = this.filterForm.value;
 
-    // --- NEW: Strict URL Parameter Mapping ---
-    // This forces the dates into the HTTP Request so the backend can't ignore them
+    // --- Pack all filters into a single object ---
     const apiParams: any = {};
     if (filters.start_date) apiParams.start_date = filters.start_date;
     if (filters.end_date) apiParams.end_date = filters.end_date;
@@ -203,7 +202,8 @@ export class StockBalanceComponent implements OnInit, OnDestroy {
     if (filters.entry_type) apiParams.entry_type = filters.entry_type;
 
     this.sites.forEach(site => {
-      this.stockService.getSiteStockSummary(site.id, filters.start_date, filters.end_date, filters.supplier_name, filters.entry_type).subscribe({
+      // Pass the single apiParams object here!
+      this.stockService.getSiteStockSummary(site.id, apiParams).subscribe({
         next: (balances: any) => {
           const tagged = balances.map((b: any) => ({ ...b, site_name: site.name, site_id: site.id }));
           this.allTimeBalances = [...this.allTimeBalances, ...tagged];
